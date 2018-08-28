cadd840a223433f6f769eb05f96156cdd80f6658
Jeff Neumann <jtneumann@hotmail.com>
2018-08-28 18:03:37 -0400

    initial commit: 25-26_Project setup including starting Header

---
+++ src/Hoc/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../Components/Header_footer/Header';
+
+const Layout = (props) => {
+    return (
+        <div>
+            <Header/>
+            {props.children}
+            footer
+
+        </div>
+    );
+};
+
+export default Layout;
---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Resources/css/app.css';
+import Layout from './Hoc/Layout';
+
+const Routes = (props) => {
+  return (
+    <Layout>
+      second line
+    </Layout>
+  )
+}
+
+export default Routes;
