4e5f97863989d6f6681e82e0d79248d01c25bd90
Jeff Neumann <jtneumann@hotmail.com>
2018-08-30 14:06:10 -0400

    32-33: Added fb and started matches blocks component

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app';
+import 'firebase/app';
+import 'firebase/database';
+
+const config = {
+    apiKey: "AIzaSyDJ6ATLvcZlu3P-voXUQERlzls2TMoeoF0",
+    authDomain: "m-city-cc64b.firebaseapp.com",
+    databaseURL: "https://m-city-cc64b.firebaseio.com",
+    projectId: "m-city-cc64b",
+    storageBucket: "m-city-cc64b.appspot.com",
+    messagingSenderId: "768222323495"
+  };
+
+  firebase.initializeApp(config);
+
+  const firebaseDB = firebase.database();
+  const firebaseMatches = firebaseDB.ref('matches');
+
+ export {
+     firebase,
+     firebaseMatches
+ }
