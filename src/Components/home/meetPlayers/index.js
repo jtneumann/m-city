4ac83bf91d62baefbce3f422e8847a0d65464b3f
Jeff Neumann <jtneumann@hotmail.com>
2018-08-31 14:08:01 -0400

    36-37:Finished MeetPlayers component

--- src/Components/home/meetPlayers/index.js
+++ src/Components/home/meetPlayers/index.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react';
 import Stripes from '../../../Resources/images/stripes.png';
 import { Tag } from '../../ui/misc';
+import Reveal from 'react-reveal/Reveal';
+import HomeCards from './cards';
 
 class MeetPlayers extends Component {
 
     state = {
-
+        show:false
     }
 
     render() {
         return (
+            <Reveal
+                fraction={0.7}
+                onReveal={()=>{
+                    this.setState({
+                        show:true
+                    })
+                }}
+            >
             <div className="home_meetplayers"
                 style={{background:`#ffffff url(${Stripes})`}}
             >
                 <div className="container">
                     <div className="home_meetplayers_wrapper">
                         <div className="home_card_wrapper">
-                            card
+                            <HomeCards
+                            show={this.state.show}
+                            />
                         </div>
                         <div className="home_text_wrapper">
                             <div>
@@ -74,6 +86,7 @@ class MeetPlayers extends Component {
                     </div>
                 </div>
             </div>
+            </Reveal>
         );
     }
 }
