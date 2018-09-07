a8d1770c1497f1b488e74307a08ae5b7c1a10796
Jeff Neumann <jtneumann@hotmail.com>
2018-09-07 13:59:11 -0400

    50:Admin_edit matches...finished form

--- src/Components/ui/formFields.js
+++ src/Components/ui/formFields.js
@@ -35,6 +35,31 @@ const FormField = ({formdata,id,change}) => {
                     </div>
                 )
             break;
+            case('select'):
+                formTemplate = (
+                    <div>
+                        { formdata.showlabel ?
+                            <div className="label-inputs">
+                                {formdata.config.label}
+                            </div>
+                        :null}
+                        <select
+                            value={formdata.value}
+                            onChange={(event)=> change({event,id})}
+                        >
+                            <option value="">Select One</option>
+                            {
+                                formdata.config.options.map((item)=>(
+                                   <option key={item.key} value={item.key}>
+                                        {item.value}
+                                   </option> 
+                                ))
+                            }
+                        </select>
+                        { showError() }  
+                    </div>
+                )
+            break;
             default:
                 formTemplate = null;
         }
